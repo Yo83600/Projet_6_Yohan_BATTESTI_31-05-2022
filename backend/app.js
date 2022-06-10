825624ac51aee55aa3c2fd41c1fbc6a239f76bc7
Yohan <yohan.battesti@gmail.com>
2022-06-10 10:49:48 +0100

    ajout de commentaires

--- backend/app.js
+++ backend/app.js
@@ -7,14 +7,17 @@ const path = require('path');
 const userRoutes = require('./routes/user');
 const saucesRoutes = require('./routes/sauces');
 
+/* Connexion à la base de donnée MongoDB */
 mongoose.connect('mongodb+srv://yo22:yoyo@cluster0.k9gdodd.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+/* Lancement du framework Express */
 const app = express();
 
+/* Middleware CORS - Ajout de headers à l'objet "response" */
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -24,7 +27,10 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+/* Rendre le dossier "images" statique */
 app.use('/images', express.static(path.join(__dirname, 'images')));
+
+/* Enregistrement des routes dans l'application */
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', saucesRoutes);
 
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -6,6 +6,7 @@ const MIME_TYPES = {
   'image/png': 'png'
 };
 
+/* Gestion des fichiers entrants dans les requêtes HTTP */
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
