ed133658284be77cdefac3fb42f01594764ba52f
Yohan <yohan.battesti@gmail.com>
2022-06-23 11:11:58 +0100

    ajout des differentes dependance pour la sécurité

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-//const helmet = require('helmet');
+const helmet = require('helmet');
 const path = require('path');
 require('dotenv').config();
 
@@ -14,7 +14,7 @@ const userRoutes = require('./routes/user');
 const saucesRoutes = require('./routes/sauces');
 
 /* Connexion à la base de donnée MongoDB */
-mongoose.connect(process.env.SECRET_DB,
+mongoose.connect(`mongodb+srv://${process.env.SECRET_USER}:${process.env.SECRET_PASSWORD}@${process.env.SECRET_DB}.k9gdodd.mongodb.net/?retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -24,7 +24,7 @@ mongoose.connect(process.env.SECRET_DB,
 const app = express();
 
 /* Middleware CORS - Ajout de headers à l'objet "response" */
-//app.use(helmet());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -37,6 +37,7 @@ app.use(express.json());
 /* Rendre le dossier "images" statique */
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+app.use(helmet());
 /* Enregistrement des routes dans l'application */
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', saucesRoutes);
